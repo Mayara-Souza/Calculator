dcd2536c9139fd949568c671b58e999651d3c3d2
Mayara-Souza <mayara.souza1523@gmail.com>
2023-02-07 20:40:51 -0300

    update display task

--- script/calculator.js
+++ script/calculator.js
@@ -1,30 +1,62 @@
-let typed = document.getElementById("typed");
-let result = document.getElementById("aux-result");
-let buttons = document.querySelectorAll(".button")
+let showTyped = document.getElementById("typed");
+let showResult = document.getElementById("aux-result");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
 let numberA;
-let operation;
+let selectedOperator;
 let numberB;
-let aux; 
+let aux=[];
 
-buttons.forEach(button =>{
-    button.addEventListener("click", ()=>{
-       updateDisplay(button);
+numbers.forEach(number =>{
+    number.addEventListener("click", ()=>{
+        displayTyping(number);
     })
 })
 
-function updateDisplay(button){
-    if(!button.classList.contains("operator")){
-        typed.innerText += button.value;
-        aux = typed.textContent;
-        console.log(typed.textContent);
-    }else{
-        numberA = aux;
-        aux = 0;
-        operation = button.value;
-        console.log("operador selecionado: "+operation);
+operators.forEach(operator =>{
+    operator.addEventListener("click", ()=>{
+        displayTyping(operator);
+    })
+})
+
+function displayTyping(cliked){
+    showTyped.innerText += cliked.value;
+
+    if(cliked.classList.contains("number")){
+        aux.push(cliked.value);
+        console.log(aux);
+    }
+
+    if(cliked.classList.contains("operator")){
+        if(cliked.value == "="){
+            numberB = aux.join("");
+            console.log("calculando...");
+            calculate(numberA,selectedOperator, numberB);
+        }else{
+            selectedOperator = cliked.value;
+            numberA = aux.join("");
+            aux.length = 0;
+            console.log("Operador: "+selectedOperator);
+        }
+    } 
+}
+
+function displayResult(result){
+        showResult.innerText = result;
+}
+
+function calculate(firstNum, operator, secNum){
+    firstNum = parseFloat(firstNum);
+    secNum = parseFloat(secNum);
+    switch(operator){
+        case "+":
+            result = (firstNum + secNum);
+            result = result.toString();
+            displayResult(result);
+            break;
     }
 }
 
-function calculate(){}
+
 function clearLastNUmber(){}
 function clearAll(){}
